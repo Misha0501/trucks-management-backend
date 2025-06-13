ea78291033a87029e00c6294f87522116eae51b5
misha0501 <misha.galenda@gmail.com>
2025-06-13 15:16:37 +0400

    updated files locations

--- TruckManagement/Program.cs
+++ TruckManagement/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 // Add services
