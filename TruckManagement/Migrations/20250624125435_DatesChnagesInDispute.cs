ddfbc4be14bc8eda54c003f6a2960bd103b64281
misha0501 <misha.galenda@gmail.com>
2025-06-24 16:54:54 +0400

    updated times

---
+++ TruckManagement/Migrations/20250624125435_DatesChnagesInDispute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TruckManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatesChnagesInDispute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OpenedAt",
+                table: "PartRideDisputes",
+                newName: "CreatedAtUtc");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ClosedAtUtc",
+                table: "PartRideDisputes",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClosedAtUtc",
+                table: "PartRideDisputes");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUtc",
+                table: "PartRideDisputes",
+                newName: "OpenedAt");
+        }
+    }
+}
